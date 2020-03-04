91f10d2f16454fd9b00507180f8817ac4dc92985
shefali-1997 <sheafaligupta@gmail.com>
2020-03-04 11:40:01 +0530

    login

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,141 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Calculator',
       theme: ThemeData(
         primarySwatch: Colors.blue
       ),
+      home: MyHomePage(),
+
     );
   }
 }
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int firstnum;
+  int secondnum;
+  String texttodisplay="";
+  String res;
+  String operatortoperform;
+  void btnclicked(
+      String btnval
+      ) {
+    if (btnval == "C") {
+      texttodisplay = "";
+      firstnum = 0;
+      secondnum = 0;
+      res = "";
+    }
+    else
+    if (btnval == "+" || btnval == "-" || btnval == "*" || btnval == "/") {
+      firstnum=int.parse(texttodisplay);
+      res="";
+      operatortoperform=btnval;
+    }
+    else if(btnval=="="){
+      secondnum=int.parse(texttodisplay);
+      if(operatortoperform=="+"){
+        res=(firstnum+secondnum).toString();
+      }
+      if(operatortoperform=="-"){
+        res=(firstnum-secondnum).toString();
+      }
+      if(operatortoperform=="*"){
+        res=(firstnum*secondnum).toString();
+      }
+      if(operatortoperform=="/"){
+        res=(firstnum/secondnum).toString();
+      }
+    }
+    else{
+      res=int.parse(texttodisplay+btnval).toString();
+    }
+    setState(() {
+      texttodisplay=res;
+    });
+  }
+  Widget custombutton(String btnval){
+    return Expanded(
+      child: OutlineButton(
+        padding: EdgeInsets.all(25.0),
+        onPressed: ()=>btnclicked(btnval),
+        child: Text("$btnval",
+        style:TextStyle(
+            fontSize: 25.0
+        )
+        ),
+      ),
+    );
+
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Calculator',
+        ),
+      ),
+      body:Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.all(10.0),
+                alignment: Alignment.topRight,
+                child: Text(
+                "$texttodisplay",
+                  style: TextStyle(
+                    fontSize: 25.0,
+                    fontWeight: FontWeight.w600,
+
+                  ),
+
+                ),
+              ),
+            ),
+            Row(
+              children: <Widget>[
+            custombutton("9"),
+            custombutton("8"),
+            custombutton("7"),
+            custombutton("+")          ],
+
+        ),
+            Row(
+              children: <Widget>[
+                custombutton("6"),
+                custombutton("5"),
+                custombutton("4"),
+                custombutton("-")          ],
+
+            ),
+            Row(
+              children: <Widget>[
+                custombutton("3"),
+                custombutton("2"),
+                custombutton("1"),
+                custombutton("*")          ],
+
+            ),
+            Row(
+              children: <Widget>[  
+                custombutton("C"),
+                custombutton("0"),
+                custombutton("="),
+                custombutton("/")          ],
+            ),
+      ],
+      ),
+    ),
+    );
+  }
+}
+
 
 
